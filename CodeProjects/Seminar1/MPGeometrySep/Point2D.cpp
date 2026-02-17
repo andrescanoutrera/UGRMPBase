92760a6dfd3927b14048816e73125eed059bd053
Andrés Cano Utrera <acu@decsai.ugr.es>
2026-02-17 14:15:04 +0100

    Uploading files

---
+++ CodeProjects/Seminar1/MPGeometrySep/Point2D.cpp
@@ -0,0 +1,11 @@
+/** 
+ * @file Point2D.cpp
+ * @author lcv
+ * @date 16 de enero de 2020, 20:06
+ */
+#include <iostream>
+#include "Point2D.h"
+
+using namespace std;
+
+
---
+++ CodeProjects/Seminar1/MPGeometrySep/Point2D.h
@@ -0,0 +1,7 @@
+/** 
+ * @file Point2D.h
+ * @author lcv
+ * @date 16 de enero de 2020, 20:03
+ */
+
+
